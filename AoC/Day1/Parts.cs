e7247bdb47f5c515d005a2cf6dced8bf6633b252
Nick Booth <nick.booth@maples.com>
2025-12-01 10:46:43 +0000

    Day 1

---
+++ AoC/Day1/Parts.cs
@@ -0,0 +1,89 @@
+﻿namespace AoC.Day1;
+
+public class Parts
+{
+    private class Dial
+    {
+        public int MaxIndex { get; set; }
+        public int CurrentIndex { get; set; }
+        public int TimesHitZero { get; set; }
+        public int TimesPassedZero { get; set; }
+
+        public void SetCurrentIndex(bool left, int amount)
+        {
+            if (left)
+            {
+                for (var i = 0; i < amount; i++)
+                {
+                    CurrentIndex -= 1;
+                    switch (CurrentIndex)
+                    {
+                        case 0:
+                            TimesPassedZero++;
+                            break;
+                        case < 0:
+                            CurrentIndex = MaxIndex;
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                for (var i = 0; i < amount; i++)
+                {
+                    CurrentIndex += 1;
+                    if (CurrentIndex > MaxIndex)
+                    {
+                        CurrentIndex = 0;
+                        TimesPassedZero++;
+                    }
+                }
+            }
+            if (CurrentIndex == 0)
+                TimesHitZero += 1;
+        }
+    }
+    public static void Part1()
+    {
+        const int startingPosition = 50;
+        const int dialSize = 99;
+        var instructions = GetInput("input.txt");
+        var dial = new Dial { MaxIndex = dialSize, CurrentIndex = startingPosition };
+
+        foreach (var instruction in instructions)
+        {
+            var direction = instruction[0];
+            var amount = int.Parse(instruction[1..]);
+            dial.SetCurrentIndex(direction == 'L', amount);
+        }
+
+        Console.WriteLine($"Hit zero {dial.TimesHitZero} times");
+    }
+
+
+    public static void Part2()
+    {
+        const int startingPosition = 50;
+        const int dialSize = 99;
+        var instructions = GetInput("input.txt");
+        var dial = new Dial { MaxIndex = dialSize, CurrentIndex = startingPosition };
+
+        foreach (var instruction in instructions)
+        {
+            var direction = instruction[0];
+            var amount = int.Parse(instruction[1..]);
+            dial.SetCurrentIndex(direction == 'L', amount);
+            Console.WriteLine($"Instruction: {instruction} Current index: {dial.CurrentIndex}");
+        }
+
+        Console.WriteLine($"Hit zero {dial.TimesPassedZero} times");
+    }
+
+    private static string[] GetInput(string inputLocation)
+    {
+        var path = Path.Combine(AppContext.BaseDirectory, "Day1", inputLocation);
+        return File.ReadAllLines(path)
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+    }
+}
---
+++ AoC/Program.cs
@@ -0,0 +1,5 @@
+﻿#region Day1
+using AoC.Day1;
+Parts.Part1();
+Parts.Part2();
+#endregion
